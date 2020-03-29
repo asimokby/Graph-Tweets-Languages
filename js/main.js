165d20d5b2b4167fc9e24bef0d9ad18c6a8f6f98
Asim Okby <asemokbey@gmail.com>
2020-03-30 01:32:30 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,67 @@
+
+
+//pie chart
+var drawChart = (langsDict) => {
+
+    dataToChart = []
+    const sum = Object.values(langsDict).reduce((a, b) => a + b, 0);
+    for (var key of Object.keys(langsDict)) {
+        const percent = (langsDict[key] / sum) * 100;
+        if (percent > 0)
+        dataToChart.push({ y: percent, label: key })
+    }
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        title: {
+            text: `Languages Percentages - ${sum} Recent Tweets`
+        },
+        data: [{
+            type: "pie",
+            startAngle: 240,
+            yValueFormatString: "##0.00\"%\"",
+            indexLabel: "{label} {y}",
+            dataPoints: dataToChart,
+        }]
+    });
+    chart.render();
+
+}
+
+
+
+
+
+
+$(document).ready(function() {
+    $('form').on('submit', function(event){  
+        $(".chartContainer").hide();  
+        $(".loader").show();
+        $(".errorOccured").hide();
+        $.ajax({
+            data: JSON.stringify({ 'username': $('#search').val() }),
+            type : 'POST',
+            contentType: "application/json",
+            // url: 'http://127.0.0.1:5000/process',
+            url: 'https://graphlangs.herokuapp.com/process',
+            error: function (XMLHttpRequest, textStatus, errorThrown){
+                $(".errorOccured").show();
+                $(".loader").hide();
+                 }
+        })
+        .done(function(data){
+            $(".loader").hide();
+            if (data.error){
+                $(".errorOccured").show();
+            } else {
+                $(".chartContainer").show();
+                $('#search').val('')
+                drawChart(JSON.parse(data))
+            }
+            
+        });
+        
+        event.preventDefault();
+    });
+
+});
+
