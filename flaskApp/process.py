da15cf6980fb3f513ddc9e9f3262645124b2981b
Asim Okby <asemokbey@gmail.com>
2021-02-09 15:31:22 +0300

    debug

--- flaskApp/process.py
+++ flaskApp/process.py
@@ -11,14 +11,14 @@ CORS(app)
 def process(): 
     if request.method == 'GET':
         return '<p>You lost? -_- Go back to the website from <a href="https://asimokby.github.io/Graph-Tweets-Languages/">here!</a><p>'
-    username = json.loads(request.data)['username']
-    if username.startswith('@'): username = username[1:]
-    langTweet = LangIdentifier(username)
-    langFreqDict = langTweet.getLangs()
+    # username = json.loads(request.data)['username']
+    # if username.startswith('@'): username = username[1:]
+    # langTweet = LangIdentifier(username)
+    # langFreqDict = langTweet.getLangs()
 
-    if not langFreqDict == -1:
-        return json.dumps(langFreqDict)
-    return jsonify({'error':'An error ocurred!'})
+    # if not langFreqDict == -1:
+    #     return json.dumps(langFreqDict)
+    # return jsonify({'error':'An error ocurred!'})
 
 if __name__ == "__main__":        
     app.run()                     
