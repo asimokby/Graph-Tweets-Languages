024b876abb7105b09262c94d8dabd94047ee49da
Asim Okby <asemokbey@gmail.com>
2021-02-09 15:57:31 +0300

    dependency upgrade

--- flaskApp/process.py
+++ flaskApp/process.py
@@ -9,17 +9,16 @@ CORS(app)
 
 @app.route('/process', methods=['GET','POST'])  
 def process(): 
-    print('This is a test')
     if request.method == 'GET':
-        return '<p>You lost? -_- Go back to the website from <a href="https://asimokby.github.io/Graph-Tweets-Languages/">here!</a><p>'
-    # username = json.loads(request.data)['username']
-    # if username.startswith('@'): username = username[1:]
-    # langTweet = LangIdentifier(username)
-    # langFreqDict = langTweet.getLangs()
+        return '<p>You lost?. Go back to the website from <a href="https://asimokby.github.io/Graph-Tweets-Languages/">here!</a><p>'
+    username = json.loads(request.data)['username']
+    if username.startswith('@'): username = username[1:]
+    langTweet = LangIdentifier(username)
+    langFreqDict = langTweet.getLangs()
 
-    # if not langFreqDict == -1:
-    #     return json.dumps(langFreqDict)
-    # return jsonify({'error':'An error ocurred!'})
+    if not langFreqDict == -1:
+        return json.dumps(langFreqDict)
+    return jsonify({'error':'An error ocurred!'})
 
 if __name__ == "__main__":        
     app.run()                     
